31be47c74230e9f0026ccb2d833675541b209be2
hr-gith <hamideh.riazat@gmail.com>
2017-03-03 00:35:24 -0500

    predictor class is added

--- src/models/Predictor.java
+++ src/models/Predictor.java
@@ -3,15 +3,55 @@ package models;
 /**
  * Created by hamideh on 24/02/2017.
  */
-import java.util.LinkedList;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Predictor {
-    public Stock stock;
-    public LinkedList<MovingAverage> movingAverageList;
+    private Stock stock;
+    private ArrayList<Integer> indicators;
+    private LinkedList<MovingAverage> movingAverageList;
 
     //getters and setters
 
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public ArrayList<Integer> getIndicators() {
+        return indicators;
+    }
+
+    public void setIndicators(ArrayList<Integer> indicators) {
+        this.indicators = indicators;
+    }
+
+    public LinkedList<MovingAverage> getMovingAverageList() {
+        return movingAverageList;
+    }
+
+    public void setMovingAverageList(){
+        if (indicators.isEmpty())
+            return;
+        movingAverageList = new LinkedList<>();
+        for (int indicator : indicators)
+            movingAverageList.add(new MovingAverage(stock.getPriceHistory(), indicator));
+    }
+
     //constructor
+    public Predictor(Stock stock, ArrayList<Integer> indicators) {
+        this.stock = stock;
+        this.indicators = indicators;
+        setMovingAverageList();
+    }
 
     /**
      * short term has the most priority
@@ -26,5 +66,30 @@ public class Predictor {
             return minMA;
         }
         return null;
+    }
+
+    public AreaChart<String, Number> getMovingAverageChart(Date from, Date to ){
+        if (indicators.size() > 0 && (movingAverageList == null || movingAverageList.isEmpty())){
+            setMovingAverageList();
+        }
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("price");
+        AreaChart<String, Number> newChart = new AreaChart<>(xAxis, yAxis);
+        newChart.setTitle("Moving average chart");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy:mm:dd");
+        for (MovingAverage ma:movingAverageList){
+            XYChart.Series<String, Number> newSeries = new XYChart.Series<>();
+            newSeries.setName(Integer.toString(ma.getIndicator()));
+            SortedMap<Date , Double> subAvgSeries = ma.getAvgSeries().subMap(from, to);
+            for (Map.Entry<Date , Double> entry : subAvgSeries.entrySet()){
+                newSeries.getData().add(new XYChart.Data<>(dateFormat.format(entry.getKey()), entry.getValue()));
+            }
+            newChart.getData().add(newSeries);
+        }
+        //newChart.intersects()
+        return newChart;
     }
 }
